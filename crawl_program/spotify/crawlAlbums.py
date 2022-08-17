af29b28f0aa487dda39d4514ddff947fdb089ff0
chenchgf <chenchgf@yonyou.com>
2022-08-18 02:04:05 +0800

    cleanup code

--- crawl_program/spotify/crawlAlbums.py
+++ crawl_program/spotify/crawlAlbums.py
@@ -9,7 +9,7 @@ from secrets import *
 # ******************************
 
 # 此处定义播放列表id
-artistId = "2QcZxAgcs2I1q7CtCkl6MI"
+artistId = "2QcZxAgcs2I1q7CtCkl6MI" # Eson Chan
 
 
 def getArtistAlbums(token, artistId, limit, offset):
@@ -53,11 +53,10 @@ for i in range(moreRequestTimes):
     allAblums.extend(moreArtistAlbums['items'])
     # printAlbums(moreArtistAlbums, offset)
 
-# print(tracklist)
 # Write json to file
-# with open('albums.json', 'w') as f:
-#     # json.dump(artistAlbums, f)
-#     json.dump(allAblums, f)
+with open('albums.json', 'w') as f:
+    # json.dump(artistAlbums, f)
+    json.dump(allAblums, f)
 
 # Simple Output
 # i = 0
@@ -69,26 +68,31 @@ for i in range(moreRequestTimes):
 #     print('Tracks: ' + str(t['total_tracks']))
 
 # Detail Output
-# count = 0
-# albumFile = open('album.txt', 'w')
-# for t in allAblums:
-#     seperation = '--------------------'
-#     count = count + 1
-#     albumName = str(count) + ': ' + t['name']
-#     albumGroup = 'Group: ' + t['album_group']
-#     albumType = 'Type:  ' + t['album_type']
-#     releaseDate = 'Date:  ' + t['release_date']
-#     tracksNum = 'Tracks: ' + str(t['total_tracks'])
-#     artists = 'Artists: '
-#     artistsList = t['artists']
-#     for i in range(len(artistsList)):
-#         artists = artists + artistsList[i]['name'] + \
-#             (', ' if i < len(artistsList) - 1 else '')
-#     print(seperation, albumName, albumGroup, albumType,
-#           releaseDate, tracksNum, artists, sep='\n')
-#     print(seperation, albumName, albumGroup, albumType,
-#           releaseDate, tracksNum, artists, sep='\n', file=albumFile)
-# albumFile.close()
+count = 0
+albumFile = open('album.txt', 'w')
+for t in allAblums:
+    seperation = '--------------------'
+    count = count + 1
+    albumName = str(count) + ': ' + t['name']
+    albumGroup = 'Group: ' + t['album_group']
+    albumType = 'Type:  ' + t['album_type']
+    releaseDate = 'Date:  ' + t['release_date']
+    tracksNum = 'Tracks: ' + str(t['total_tracks'])
+    artists = 'Artists: '
+    artistsList = t['artists']
+    for i in range(len(artistsList)):
+        artists = artists + artistsList[i]['name'] + \
+            (', ' if i < len(artistsList) - 1 else '')
+    print(seperation, albumName, albumGroup, albumType,
+          releaseDate, tracksNum, artists, sep='\n')
+    print(seperation, albumName, albumGroup, albumType,
+          releaseDate, tracksNum, artists, sep='\n', file=albumFile)
+albumFile.close()
+
+# Filter ablums to album and single
+# for album in allAblums[::-1]:
+#     if (album['album_type'] != 'album' and album['album_type'] != 'single'):
+#         allAblums.remove(album)
 
 print('--------------------')
 print('Album Statistic:')
@@ -111,7 +115,6 @@ for t in allAblums:
         albumGroupTypeStat[albumGroupType] = 1
     else:
         albumGroupTypeStat[albumGroupType] = albumGroupTypeStat[albumGroupType] + 1
-
 print(albumGroupStat.items())
 print(albumTypeStat.items())
 print(albumGroupTypeStat.items())
